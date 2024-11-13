e8de0498b440316f1fa45fb994642a87ccfc8b58
nastia <gnastenka08@gmail.com>
2024-11-13 09:11:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
 print ("Дан объем шара X куб. ед. "
 "Найдите радиус фигуры.")
 x = int (input ("Введите x" ))
-import math
 r = (3 * (int(x) ** (1/3))) / ( 4 * math.pi)
 print ("Радиус равен", float(r))
